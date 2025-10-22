88e58ee337696866d4566f7780b3f005e3c6e59e
Zafarali Tolibov <zafaralit67@gmail.com>
2025-10-22 14:35:28 -0500

    Added an example for binary search tree to reference back later

--- ds-a-java/src/Main.java
+++ ds-a-java/src/Main.java
@@ -1,10 +1,20 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Main {
     public static void main(String[] args) {
-        Queue<String> queue = new LinkedList<>();
 
+        BinarySearchTree tree = new BinarySearchTree();
 
+        tree.insert(new Node(5));
+        tree.insert(new Node(1));
+        tree.insert(new Node(9));
+        tree.insert(new Node(2));
+        tree.insert(new Node(7));
+        tree.insert(new Node(3));
+        tree.insert(new Node(6));
+        tree.insert(new Node(4));
+        tree.insert(new Node(8));
+
+        tree.display();
+
+        System.out.println(tree.search(9));
     }
 }
